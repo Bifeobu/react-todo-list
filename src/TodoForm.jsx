948155dcf937d97e425701a9f69dc084f89bf122
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-23 22:15:47 -0400

    todo function usestate

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 
 function TodoForm({ addTodo }) {
+    const [text, setText] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (text.trim()) {
+      addTodo(text);
+      setText('');
+    }
+  };
 
     return(
         <>
