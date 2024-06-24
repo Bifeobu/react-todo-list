81d37b618f2045536092c1165fd8f97a8ea1d55a
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-23 22:46:46 -0400

    last changes to the todo form

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -3,16 +3,25 @@ import { useState } from 'react'
 function TodoForm({ addTodo }) {
     const [text, setText] = useState('');
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (text.trim()) {
-      addTodo(text);
-      setText('');
-    }
-  };
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (text.trim()) {
+            addTodo(text);
+            setText('');
+        }
+    };
 
-    return(
+    return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={text}
+                    onChange={e => setText(e.target.value)}
+                />
+                <button type="submit">Add Todo</button>
+            </form>
+
         </>
     )
 }
